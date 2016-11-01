4da527a9fd882b7c74a21ade8a519fcdac73cceb
Stefan Wegener <stefan@stefan-wegener.eu>
2016-11-01 13:07:58 +0100

    perform_installation.py is more chatty now.

--- scripts/perform_installation.py
+++ scripts/perform_installation.py
@@ -9,6 +9,7 @@ that should be used.
 
 HECTOR_TRACKER_INSTALL_PATH = sys.argv[1]
 
+
 class cd:
     """Context manager for changing the current working directory"""
     def __init__(self, newPath):
@@ -22,23 +23,25 @@ class cd:
         os.chdir(self.savedPath)
 
 with cd(HECTOR_TRACKER_INSTALL_PATH):
-    result = subprocess.check_output('./update.sh')
-    print("Output of ./update.sh: \n" + result)
-    if '[build] Failed: No packages failed.' not in result:
+    try:
+        result = subprocess.check_output('./update.sh', stderr=subprocess.STDOUT)
+        print("Output of ./update.sh: \n" + result)
+        if '[build] Failed: No packages failed.' not in result:
+            sys.exit([1])
+
+        optional_requirements = ['gazebo_sim.rosinstall', 'gui.rosinstall',
+                                 'integration_testing.rosinstall', 'icp_mapping.rosinstall']
+        for optional_requirement in optional_requirements:
+            commandList = ('wstool merge optional_installs/' + optional_requirement).split(' ')
+            result = subprocess.check_output(commandList, stderr=subprocess.STDOUT)
+            print("Output of wstool merge:\n" + result )
+
+        result = subprocess.check_output(['wstool', 'update'], stderr=subprocess.STDOUT)
+        print("Output of wstool update: \n" + result)
+        result = subprocess.check_output(['catkin', 'build'], stderr=subprocess.STDOUT)
+        print("Output of catkin build: \n" + result)
+        if '[build] Failed: No packages failed.' not in result:
+            sys.exit([1])
+    except subprocess.CalledProcessError as error:
+        print("Error while calling a command in perform_installation. Output: " + error.output)
         sys.exit([1])
-
-    optional_requirements = ['gazebo_sim.rosinstall', 'gui.rosinstall', 
-                             'integration_testing.rosinstall', 'icp_mapping.rosinstall']
-    for optional_requirement in optional_requirements:
-        commandList = ('wstool merge optional_installs/' + optional_requirement).split(' ')
-        result = subprocess.check_output(commandList)
-        print("Output of wstool merge:\n" + result )
-
-    result = subprocess.check_output(['wstool', 'update'])
-    print("Output of wstool update: \n" + result)
-    result = subprocess.check_output(['catkin', 'build'])
-    print("Output of catkin build: \n" + result)
-    if '[build] Failed: No packages failed.' not in result:
-        sys.exit([1])
-
-
